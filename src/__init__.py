13f8606216306eb488e624414cabdea32338be7c
AdaWorldAPI <jan@exo.red>
2026-01-28 14:12:47 +0100

    Add src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+ITIL-NEXT Engine
+
+Core ticket system with:
+- Immutable ownership (anti-Valuemation)
+- Envelope pattern (Amdocs sub-case)
+- Empowerment tiers (Sony pattern)
+- Dynamic priority scoring
+- HubSpot-style timeline
+"""
+
+__version__ = "0.1.0"
